ff36444ca524bbf7ec53069a09d816d70d3389bb
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-02-17 15:33:24 +0100

    Added main router routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Gallery from "./components/Gallery/Gallery";
 import Menu from "./components/Menu/Menu";
 import data from "./data";
 
 function App() {
-  const categories = {
-    personal: [
-      "copilot",
-      "desert",
-      "facades",
-      "la familie",
-      "on a rock",
-      "polish",
-      "rouge",
-      "stairs",
-      "essentials",
-      "places",
-      "wallpapers",
-      "arch",
-    ],
-    locations: ["drone", "exterior", "interior", "car", "nature"],
+  const getCategories = () => {
+    const categories = {
+      personal: [],
+      location: [],
+    };
+
+    for (const categorie in data.personal) {
+      categories.personal.push(categorie);
+    }
+
+    for (const categorie in data.location) {
+      categories.location.push(categorie);
+    }
+
+    return categories;
   };
+
+  const categoriesList = getCategories();
 
   return (
     <div className="app">
-      <Menu categoriesList={categories} />
+      <Menu categoriesList={categoriesList} />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Navigate to="/personal/essential" />} />
+        <Route path="/personal" element={<Gallery theme={data.personal} />} />
+        <Route path="/location" element={<Gallery theme={data.location} />} />
         <Route
-          path="/personal"
-          element={<Gallery collection={data.personal} />}
+          path="/personal/:collection"
+          element={<Gallery theme={data.personal} />}
         />
         <Route
-          path="/location"
-          element={<Gallery collection={data.location} />}
+          path="/location/:collection"
+          element={<Gallery theme={data.location} />}
         />
+        <Route path="*" element={<Navigate to="/personal/essential" />} />
       </Routes>
     </div>
   );
